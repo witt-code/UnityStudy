b9dbf3212b0d16f0908c8d6572d216d89f6943cc
witt-code <62375811+witt-code@users.noreply.github.com>
2020-07-08 18:41:46 +0900

    Add files via upload

---
+++ EnemyMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public int nextMove;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 5);
+    }
+
+    void FixedUpdate()
+    {
+        //Move
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        //Platform Check        
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.3f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("platform"));
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+    }
+    void Think()
+    {
+        //Set Next Active
+        nextMove = Random.Range(-1, 2); //Random클래스 내 Range 함수는 (최소값, 최대값)을 받는데, 최소값은 범위에 포함, 최대값은 포함 x
+        
+        //Sprite Animation
+        anim.SetInteger("WalkSpeed", nextMove);
+        
+        //Flip Sprite
+        if (nextMove != 0)
+        {
+            spriteRenderer.flipX = nextMove == 1;
+        }
+
+        //Recursive
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+
+    }
+
+    void Turn()
+    {
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
+        CancelInvoke();
+        Invoke("Think", 5);
+        Debug.Log("경고! 낭떠러지");
+    }
+
+}
